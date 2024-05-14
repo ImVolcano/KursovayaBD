3133a217e6dbd86537d51ee9dd963827c51f0a00
ImVolcano <maks_kasimov@internet.ru>
2024-05-14 13:56:14 +0300

    Created empty ASP.Net API project

---
+++ WebAPI/WebAPI/Controllers/DataController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class DataController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok();
+        }
+    }
+}
